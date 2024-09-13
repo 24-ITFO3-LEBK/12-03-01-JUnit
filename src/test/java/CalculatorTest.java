bbeca27feb4f3ba140358f32ec117a4ba22bf56c
phamhuulebk <phamhuu@lebk247.de>
2024-09-13 10:57:01 +0200

    added divide test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
@@ -18,5 +19,18 @@ public class CalculatorTest {
     @Test
     public void testMultiply() {
         assertEquals(6, calculator.multiply(2, 3), "2 * 3 sollte 6 ergeben");
+    }
+
+    @Test
+    public void testDivide() {
+        assertEquals(6, calculator.divide(30, 5), "30 / 5 sollte 6 ergeben");
+    }
+
+    @Test
+    public void testDivideByZero() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(2,0));
+        String expectedMessages = "Cannot divide by zero.";
+        String actuallyMessages = exception.getMessage();
+        assertTrue(actuallyMessages.contains(expectedMessages));
     }
 }
