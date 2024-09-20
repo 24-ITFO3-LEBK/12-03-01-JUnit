7855d7a60ea4881c39ba8adbd4f7b43b12325c6f
phamhuulebk <phamhuu@lebk247.de>
2024-09-20 10:55:32 +0200

    Aufgabe 1-4 Added Fibonacci method and test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,10 +1,30 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.time.Duration;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private final Calculator calculator = new Calculator();
+    private Calculator calculator;
+    @BeforeEach
+    void init(){
+        System.out.println("Test wird gestartet");
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void endTest(){
+        System.out.println("Test beendet");
+        calculator = null;
+    }
+
 
     @Test
     public void testAdd() {
@@ -31,5 +51,54 @@ public class CalculatorTest {
         Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(2,0));
         String actuallyMessages = exception.getMessage();
         assertTrue(actuallyMessages.contains(Calculator.CANNOT_DIVIDE_BY_ZERO));
+    }
+
+    @Test
+    public void testEmptyFibilist(){
+        //Return a adress that has a empty list.
+        //assertEquals(new ArrayList<>(),calculator.generateFibonacci(0),"should return empty list");
+        int[] actually ={};
+        //assertEquals(actually,calculator.generateFibonacci(0),"should return empty list");
+        //To actaully test it you have to do somehting like this
+        int[] expectedList = calculator.generateFibonacci(0);
+        assertEquals(actually.length,expectedList.length,"should return empty list");
+    }
+
+    @Test
+    public void testGenerateFibonacciList(){
+        int n = 6;
+        int[] expectedList = calculator.generateFibonacci(n);
+        for (int i = 0 ; i< n ; i++){
+            assertEquals(n,expectedList.length, "a not empty list is returned");
+        }
+    }
+
+    @Test
+    public void testFibonacci(){
+        int[] actually ={0,1,1,2,3,5};
+        int n = 6;
+        int[] expectedList = calculator.generateFibonacci(n);
+        for (int i = 0 ; i< n ; i++){
+            assertEquals(actually[i],expectedList[i], "should be"+ actually[i]);
+        }
+    }
+
+    @Test
+    public void testGenerateSpeedOfFibonacciList(){
+        int n = 40;
+        int[] expectedList = calculator.generateFibonacci(n);
+        for (int i = 0 ; i< n ; i++){
+           assertTimeout(Duration.ofMillis(1),() -> {
+               calculator.generateFibonacci(n);
+           });
+        }
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0,1,10,20})
+    void testParameterizedFiboTest(int n){
+        int number = n;
+        int[] expected =calculator.generateFibonacci(number);
+        assertEquals(n, expected.length,"Die Anzahl der Elemente = Länge der Liste");
     }
 }
