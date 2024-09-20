20a62438e33a81d0105cf977de185f70945b69e5
BiggestChongus <HenrioKar@lebk247.de>
2024-09-20 11:38:24 +0200

    generateFibonacci und dazugehörige Tests implementiert

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,23 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private final Calculator calculator = new Calculator();
+    private Calculator calculator;
+    @BeforeEach
+    void init(){
+        calculator = new Calculator();
+    }
+
+    @AfterEach
+    void teardown() {
+        System.out.println("Test abgeschlossen");
+    }
 
     @Test
     public void testAdd() {
@@ -29,4 +43,33 @@ public class CalculatorTest {
     public void testDivideException() {
         assertThrows(IllegalArgumentException.class, () -> calculator.divide(3,0), "Division durch Null soll einen Fehler werfen.");
     }
-}
+
+    @Test
+    public void testFibNotNull() {
+        assertTrue(calculator.generateFibonacci(1).length > 0, "Die Fibunaccifolge sollte nicht leer sein.");
+    }
+
+    @Test
+    public void testFibNull() {
+        assertEquals( 0, calculator.generateFibonacci(0).length, "Die Fibunaccifolge sollte nicht leer sein.");
+    }
+
+    @Test
+    public void testFirstTwoValues(){
+        int[] fibunacci = calculator.generateFibonacci(2);
+        assertEquals(fibunacci[0], 0, "Der erste Wert der Fibunaccireihe sollte Null sein");
+        assertEquals(fibunacci[1], 1, "Der zweite Wert der Fibunaccireihe sollte Eins sein");
+    }
+
+    @Test
+    public void testFibAll(){
+        int[] fibunacci = calculator.generateFibonacci(45000);
+        for(int i = fibunacci.length-1; i>1; i--){
+            assertEquals(fibunacci[i-1]+fibunacci[i-2],fibunacci[i]);
+        }
+    }
+
+    @Test
+    public void fibRuntimeTest(){
+        assertTimeout(Duration.ofSeconds(1), ()->calculator.generateFibonacci(45000), "Hohe Zahlen sollten innerhalb von einer Sekunde zur Fibonaccireihe umgewandelt werden");    }
+    }
