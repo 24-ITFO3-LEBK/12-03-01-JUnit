b21899c3774e7b05adddc5c0ae3f4c47b18cf470
Marcel <ewaldmar@lebk247.de>
2024-09-27 10:39:16 +0200

    Add generateFibonacci tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,23 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import java.time.Duration;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private final Calculator calculator = new Calculator();
+    private Calculator calculator;
+
+    @BeforeEach
+    public void createNewTestObject() {
+        calculator = new Calculator();
+        System.out.println("Starting test");
+    }
+
+    @AfterEach
+    public void finishTest() {
+        System.out.println("Finished test");
+    }
 
     @Test
     public void testAdd() {
@@ -21,16 +35,43 @@ public class CalculatorTest {
     }
 
     @Test
-    public void testDivideShouldReturnExpectedResult()
+    public void testDivide_ShouldReturnExpectedResult()
     {
         int expectedResult = 5;
         assertEquals(expectedResult, calculator.divide(10, 2), "10 / 2 should return 5");
     }
 
     @Test
-    public void testDivideWithZeroShouldThrow()
+    public void testDivideWithZero_ShouldThrow()
     {
         Class<IllegalArgumentException> expectedException = IllegalArgumentException.class;
         assertThrowsExactly(expectedException, () -> calculator.divide(10, 0), "Number can not be divided by 0");
+    }
+
+    @Test
+    public void testGenerateFibonacci_ShouldReturnExpectedResult() {
+        int[] testResult = calculator.generateFibonacci(5);
+        int[] expectedResult = { 0, 1, 1, 2, 3 };
+        assertNotNull(testResult);
+        assertArrayEquals(testResult, expectedResult);
+    }
+
+    @Test
+    public void testGenerateFibonacci_WithZero_ShouldReturnEmptyResult() {
+        int[] testResult = calculator.generateFibonacci(0);
+        int[] expectedResult = {};
+        assertArrayEquals(testResult, expectedResult);
+    }
+
+    @Test
+    public void testGenerateFibonacci_WithHighNumber_ShouldReturnExpectedResultWithinTime() {
+        int[] expectedResult = { 0, 1, 1, 2, 3};
+        Duration expectedDuration = Duration.ofSeconds(1);
+
+        assertTimeout(expectedDuration, () -> {
+            int[] testResult = calculator.generateFibonacci(100);
+            int[] slicedTestResult = Arrays.copyOfRange(testResult, 0, 5);
+            assertArrayEquals(slicedTestResult, expectedResult);
+        });
     }
 }
