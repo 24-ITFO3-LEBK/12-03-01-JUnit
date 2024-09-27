0ef5b81411b64e017696368782e7cb4335602f63
darouimon <darouimon@lebk247.de>
2024-09-27 09:17:58 +0200

    Aufgabe Unittest erweitern

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,9 +1,15 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class CalculatorTest {
 
-    private final Calculator calculator = new Calculator();
+    private Calculator calculator;
 
     @Test
     public void testAdd() {
@@ -39,4 +45,50 @@ public class CalculatorTest {
     public void testDivideThrowsNoException(){
          assertDoesNotThrow(()->calculator.divide(6,2));
     }
+
+    @Test
+    public void testGenerateFibonacciNGreaterZero(){
+        assertNotNull(calculator.generateFibonacci(5));
+
+    }
+
+    @Test
+    public void testGenerateFibonacciNIsZero(){
+        boolean isEmpty = isEmpty(calculator.generateFibonacci(0));
+        assertTrue(isEmpty);
+    }
+
+    private boolean isEmpty(int[] array){
+        return array.length == 0;
+    }
+
+    @Test
+    public void testGenerateFibonacciFirstTwoValues(){
+        int[] fib = calculator.generateFibonacci(5);
+                assertEquals(0,fib[0]);
+                assertEquals(1,fib[1]);
+    }
+
+    @Test
+    public void testGenerateFibonacciArrayEquals(){
+        int[] expectedArray = new int[]{0,1,1,2,3};
+        assertArrayEquals(expectedArray,calculator.generateFibonacci(5));
+
+    }
+
+    @Test
+    public void testGenerateFibonacciTimeout(){
+        assertTimeout(Duration.ofSeconds(1),()-> calculator.generateFibonacci(3000));
+    }
+
+    @BeforeEach
+    public void beforeEachMethod(){
+         calculator = new Calculator();
+    }
+
+    @AfterEach
+    public void afterEachMethod(){
+       System.out.println("Test abgeschlossen");
+    }
+
 }
