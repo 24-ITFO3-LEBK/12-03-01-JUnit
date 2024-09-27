8c4558d39a98d3e17fd71a114cbe509440277ae8
phamhuulebk <phamhuu@lebk247.de>
2024-09-27 10:55:17 +0200

    added Potenz , factorial und isPrime rechner

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -13,23 +13,71 @@ public class Calculator {
         return a * b;
     }
 
-    public double divide(int a, int b) {
+    public double divide(int a, double b) {
         if (b == 0) {
             throw new IllegalArgumentException(CANNOT_DIVIDE_BY_ZERO);
         }
         return (double) a / b;
     }
 
-    public int[] generateFibonacci(int n ){
-        if(n <= 0) return new int []{};
+    public int[] generateFibonacci(int n) {
+        if (n <= 0) return new int[]{};
         int[] fib = new int[n];
         fib[0] = 0;
-        if( n > 1){
-            fib[1]=1;
-            for (int i = 2; i<n; i++){
-                fib[i] = fib[i-1] + fib[i-2];
+        if (n > 1) {
+            fib[1] = 1;
+            for (int i = 2; i < n; i++) {
+                fib[i] = fib[i - 1] + fib[i - 2];
             }
         }
         return fib;
+    }
+
+    public double potenz(double a, int b) {
+        double erg;
+        if (b == 0) erg = 1;
+        else if (b < 0) {
+            int posPotenz = b * -1;
+            erg = divide(1, potenz(a, posPotenz));
+        } else {
+            erg = a;
+            for (int i = 1; i < b; i++) {
+                erg *= a;
+            }
+        }
+        return erg;
+    }
+
+    public int factorial(int i) {
+        int erg = 1;
+        for (int j = 0; j < i; j++) {
+            erg = erg * (j+1);
+        }
+        return erg;
+    }
+
+    public int gcd(int a, int b) {
+        int i;
+        if (a < b)
+            i = a;
+        else
+            i = b;
+        for (i = i; i > 1; i--) {
+            if (a % i == 0 && b % i == 0)
+                return i;
+        }
+        return 1;
+    }
+
+    public boolean isPrime(double a) {
+        if (a < 2) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(a); i++) {
+            if (a % i == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,26 +1,25 @@
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
     private Calculator calculator;
+
     @BeforeEach
-    void init(){
+    void init() {
         System.out.println("Test wird gestartet");
         calculator = new Calculator();
     }
 
     @AfterEach
-    void endTest(){
+    void endTest() {
         System.out.println("Test beendet");
         calculator = null;
     }
@@ -48,64 +47,126 @@ public class CalculatorTest {
 
     @Test
     public void testDivideByZero() {
-        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(2,0));
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> calculator.divide(2, 0));
         String actuallyMessages = exception.getMessage();
         assertTrue(actuallyMessages.contains(Calculator.CANNOT_DIVIDE_BY_ZERO));
     }
 
     @Test
-    public void testEmptyFibilist(){
+    public void testEmptyFibilist() {
         //Return a adress that has a empty list.
         //assertEquals(new ArrayList<>(),calculator.generateFibonacci(0),"should return empty list");
         //assertEquals(actually,calculator.generateFibonacci(0),"should return empty list");
         //To actaully test it you have to do somehting like this
         int[] actually = calculator.generateFibonacci(0);
-        assertEquals(0,actually.length,"should return empty list");
+        assertEquals(0, actually.length, "should return empty list");
     }
 
     @Test
-    public void testGenerateFibonacciList(){
+    public void testGenerateFibonacciList() {
         int n = 6;
         int[] actuallyList = calculator.generateFibonacci(n);
-        assertEquals(n,actuallyList.length, "a not empty list is returned");
+        assertEquals(n, actuallyList.length, "a not empty list is returned");
     }
 
     @Test
-    public void testFibonacci(){
-        int[] actually ={0,1,1,2,3,5};
+    public void testFibonacci() {
+        int[] actually = {0, 1, 1, 2, 3, 5};
         int n = 6;
         int[] expectedList = calculator.generateFibonacci(n);
-        for (int i = 0 ; i< n ; i++){
-            assertEquals(actually[i],expectedList[i], "should be"+ actually[i]);
+        for (int i = 0; i < n; i++) {
+            assertEquals(actually[i], expectedList[i], "should be" + actually[i]);
         }
     }
 
     @Test
-    public void testGenerateSpeedOfFibonacciList(){
+    public void testGenerateSpeedOfFibonacciList() {
         int n = 100;
-        for (int i = 0 ; i< n ; i++){
+        for (int i = 0; i < n; i++) {
             int finalI = i;
-            assertTimeout(Duration.ofMillis(1),() -> {
-               calculator.generateFibonacci(finalI);
-           });
+            assertTimeout(Duration.ofSeconds(1), () -> {
+                calculator.generateFibonacci(finalI);
+            });
         }
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {0,1,10,20})
-    void testParameterizedFiboTest(int n){
-        int[] actually =calculator.generateFibonacci(n);
-        assertEquals(n, actually.length,"Die Anzahl der Elemente = Länge der Liste");
+    @ValueSource(ints = {0, 1, 10, 20})
+    void testParameterizedFiboTest(int n) {
+        int[] actually = calculator.generateFibonacci(n);
+        assertEquals(n, actually.length, "Die Anzahl der Elemente = Länge der Liste");
     }
 
     @ParameterizedTest
     @ValueSource(ints = 10)
-    void testParameterizedFiboTestWithOneValue(int n){
-        int[] expected = {0,1,1,2,3,5,8,13,21,34};
-        int[] actualList =calculator.generateFibonacci(n);
-        assertEquals(actualList.length, expected.length,"Gleiche Menge an Elementen");
-        for (int i = 0 ; i< n ; i++){
-            assertEquals(actualList[i],expected[i], "should be"+ actualList[i]);
+    void testParameterizedFiboTestWithOneValue(int n) {
+        int[] expected = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34};
+        int[] actualList = calculator.generateFibonacci(n);
+        assertEquals(actualList.length, expected.length, "Gleiche Menge an Elementen");
+        for (int i = 0; i < n; i++) {
+            assertEquals(actualList[i], expected[i], "should be" + actualList[i]);
         }
+    }
+
+    @Test
+    public void testPotenz() {
+        assertEquals(8, calculator.potenz(2, 3), "2^3 = 8");
+    }
+
+    @Test
+    public void testPotenzZero() {
+        assertEquals(1, calculator.potenz(5, 0), "5^0 = 1");
+    }
+
+    @Test
+    public void testNegativePotenz() {
+        assertEquals(0.125, calculator.potenz(2, -3), "2^-3 = 0.125");
+    }
+
+    @Test
+    public void testFakultaet() {
+        assertEquals(120, calculator.factorial(5), "!5 = 120");
+    }
+
+    @Test
+    public void testFakultaet2() {
+        assertEquals(3628800, calculator.factorial(10), "!10 = 3.628.800");
+    }
+
+    @Test
+    public void testFakultaetNull() {
+        assertEquals(1, calculator.factorial(0), "!0 = 1");
+    }
+
+    @Test
+    public void testGGT() {
+        assertEquals(6, calculator.gcd(54,24), "der ggt ist 6");
+    }
+    public void testGGT2() {
+        assertEquals(54, calculator.gcd(54,432), "der ggt ist 54");
+    }
+    public void testGGT3() {
+        assertEquals(1, calculator.gcd(54,33), "der ggt ist 1");
+    }
+
+    @Test
+    public void testIsPrime() {
+        assertTrue( calculator.isPrime(11), "wahr");
+    }
+    @Test
+    public void testIsNotPrime() {
+        assertFalse( calculator.isPrime(4), "wahr");
+    }
+    @Test
+    public void testIsPrimeTwo() {
+        assertTrue( calculator.isPrime(97), "wahr");
+    }
+    @Test
+    public void testIsPrime3() {
+        assertTrue( calculator.isPrime(3), "wahr");
+    }
+    @Test
+    public void testIsPrime4() {
+        assertTrue( calculator.isPrime(2), "wahr");
     }
 }
