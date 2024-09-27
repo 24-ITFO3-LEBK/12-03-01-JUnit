5d55a0f116de90ff0b2e5dc83b5050fc54c413c3
Marcel <ewaldmar@lebk247.de>
2024-09-27 12:16:48 +0200

    Fix tests

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,22 +1,22 @@
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import java.time.Duration;
 import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
 
-    private Calculator calculator;
+    private final Calculator calculator = new Calculator();
 
     @BeforeEach
-    public void createNewTestObject() {
-        calculator = new Calculator();
-        System.out.println("Starting test");
+    public void InitTest() {
+        System.out.println("Starting test..");
     }
 
     @AfterEach
-    public void finishTest() {
-        System.out.println("Finished test");
+    public void CleanUpTest() {
+        System.out.println("Finished test!");
     }
 
     @Test
@@ -71,7 +71,47 @@ public class CalculatorTest {
         assertTimeout(expectedDuration, () -> {
             int[] testResult = calculator.generateFibonacci(100);
             int[] slicedTestResult = Arrays.copyOfRange(testResult, 0, 5);
+
             assertArrayEquals(slicedTestResult, expectedResult);
         });
+    }
+
+    @Test
+    public void testPower_ShouldReturnExpectedResults() {
+        assertEquals(8, calculator.power(2, 3));
+        assertEquals(1, calculator.power(5, 0));
+        assertEquals(0.125, calculator.power(2, -3));
+    }
+
+    @Test
+    public void testFactorial_ShouldReturnExpectedResults() {
+        assertEquals(120, calculator.factorial(5));
+        assertEquals(3_628_800, calculator.factorial(10));
+    }
+
+    @Test
+    public void testGcd_ShouldReturnExpectedResults() {
+        assertEquals(6, calculator.gcd(54, 24));
+        assertEquals(1, calculator.gcd(17, 13));
+    }
+
+    @Test
+    public void isPrime_ShouldReturnExpectedResults() {
+        assertTrue(calculator.isPrime(2));
+        assertTrue(calculator.isPrime(11));
+        assertFalse(calculator.isPrime(4));
+        assertFalse(calculator.isPrime(9));
+    }
+
+    @Test
+    public void sqrt_ShouldReturnExpectedResults() {
+        assertEquals(3.0, calculator.sqrt(9));
+        assertEquals(1.4, calculator.sqrt(2));
+    }
+
+    @Test
+    public void sqrt_WithNegativeInput_ShouldThrow() {
+        Class<IllegalArgumentException> expectedException = IllegalArgumentException.class;
+        assertThrows(expectedException, () -> calculator.sqrt(-1));
     }
 }
