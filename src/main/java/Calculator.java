d015bca89e26196fe4452bbd6c097ed0d749801a
WienhoSte <wienhoste@lebk247.de>
2024-10-11 11:25:56 +0200

    Calc

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,8 @@
+import java.util.Arrays;
+import java.util.OptionalDouble;
+import java.util.OptionalInt;
+import java.util.stream.Collectors;
+
 public class Calculator {
     public int add(int a, int b) {
         return a + b;
@@ -30,4 +35,82 @@ public class Calculator {
         }
         return fib;
     }
+
+    public double power(int base, int exponent) {
+        return Math.pow(base, exponent);
+    }
+
+    public int factorial(int num) {
+        if (num < 0) {
+            throw new IllegalArgumentException("Eingabewert darf nicht negativ sein.");
+        } else if (num == 0) return 1;
+        int sum = 0;
+        while (num > 1) {
+            if (sum == 0) {
+                sum = num * (num - 1);
+            } else {
+                sum = sum * (num - 1);
+            }
+            num--;
+        }
+        return sum;
+    }
+
+    public int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b, a % b);
+    }
+
+    public boolean isPrime(int num) {
+        if (num == 0 || num == 1) {
+            return false;
+        } else if (num == 3) {
+            return true;
+        }
+        int divisor = num / 2;
+        boolean isPrime = false;
+        while (divisor > 1) {
+            if (num % divisor != 0) {
+                isPrime = true;
+                divisor--;
+            } else {
+                isPrime = false;
+                break;
+            }
+        }
+        return isPrime;
+    }
+
+    public double sqrt(double i) {
+        if (i < 0) {
+            throw new IllegalArgumentException("Negative Zahlen sind ungültig.");
+        }
+        return Math.sqrt(i);
+    }
+
+    public int summe(int[] ganzzahlen){
+        return Arrays.stream(ganzzahlen).sum();
+    }
+
+    public OptionalDouble durchschnitt(int[] ganzzahlen){
+        return Arrays.stream(ganzzahlen).average();
+    }
+
+    public OptionalInt maximalwert(int[] ganzzahlen){
+        return Arrays.stream(ganzzahlen).max();
+    }
+
+    public OptionalInt minimalwert(int[] ganzzahlen){
+        return Arrays.stream(ganzzahlen).min();
+    }
+
+    public int[] geradeZahlen(int[] ganzzahlen) {
+        return Arrays.stream(ganzzahlen).filter(n -> n % 2 == 0).toArray();
+    }
+
+    public double[] sqrt2(double[] ganzzahlen){
+        return Arrays.stream(ganzzahlen).filter(n -> n % 2 == 0).map(this::sqrt).toArray();
+    }
+
+  
 }
